c79a34c2bdf5d8ffefc14366388782a46f383c10
Justin Yoo <justin.yoo@aliencube.com>
2017-10-26 22:13:25 +1100

    Add test project for Services

---
+++ test/AglCodingTest.Services.Tests/Fixtures/ServiceFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace AglCodingTest.Services.Tests.Fixtures
+{
+    /// <summary>
+    /// This represents the fixture entity to test services.
+    /// </summary>
+    public class ServiceFixture : IDisposable
+    {
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceFixture"/> class.
+        /// </summary>
+        public ServiceFixture()
+        {
+            this.Fixture = new Fixture().Customize(new AutoMoqCustomization());
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IFixture"/> instance.
+        /// </summary>
+        public IFixture Fixture { get; }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (this._disposed)
+            {
+                return;
+            }
+
+            this._disposed = true;
+        }
+    }
+}
