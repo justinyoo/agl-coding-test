ee203a5b8a4370c6413483a257e9556f6edc543d
Justin Yoo <justin.yoo@aliencube.com>
2017-10-26 23:59:42 +1100

    Add service layer for payload loading

---
+++ src/AglCodingTest.Services/IAglPayloadLoadingService.cs
@@ -0,0 +1,9 @@
+﻿namespace AglCodingTest.Services
+{
+    /// <summary>
+    /// This provides interfaces to the <see cref="AglPayloadLoadingService"/> class.
+    /// </summary>
+    public interface IAglPayloadLoadingService : IService
+    {
+    }
+}
---
+++ src/AglCodingTest.Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using AglCodingTest.Services.ServiceOptions;
+
+namespace AglCodingTest.Services
+{
+    /// <summary>
+    /// This provides interfaces to all classes in the service layer.
+    /// </summary>
+    public interface IService : IDisposable
+    {
+        /// <summary>
+        /// Invokes the service.
+        /// </summary>
+        /// <typeparam name="TOptions">Type of service options.</typeparam>
+        /// <param name="options"><see cref="TOptions"/> instance.</param>
+        /// <returns>Returns <see cref="Task"/>.</returns>
+        Task InvokeAsync<TOptions>(TOptions options)
+            where TOptions : ServiceOptionsBase;
+    }
+}
