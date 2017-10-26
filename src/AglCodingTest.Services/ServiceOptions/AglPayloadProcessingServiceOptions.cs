678eb6ea4c347c13feaf68dd18b9fa823180d2da
Justin Yoo <justin.yoo@aliencube.com>
2017-10-27 10:03:10 +1100

    Add service layer to process payload

---
+++ src/AglCodingTest.Services/ServiceOptions/AglPayloadProcessingServiceOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using AglCodingTest.Models;
+
+namespace AglCodingTest.Services.ServiceOptions
+{
+    /// <summary>
+    /// This represents the options entity for <see cref="AglPayloadProcessingService"/>.
+    /// </summary>
+    public class AglPayloadProcessingServiceOptions : ServiceOptionsBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AglPayloadProcessingServiceOptions"/> class.
+        /// </summary>
+        public AglPayloadProcessingServiceOptions()
+        {
+            this.People = new List<Person>();
+            this.Groups = new List<string>();
+        }
+
+        /// <summary>
+        /// Gets or sets the list of <see cref="Person"/> entities.
+        /// </summary>
+        public List<Person> People { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of group of pets.
+        /// </summary>
+        public List<string> Groups { get; set; }
+    }
+}
